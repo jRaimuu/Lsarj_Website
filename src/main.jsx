16e7f2e3bf96cfe0204f42d7a3018a91a5c997d0
jRaimuu <liamsarj4@gmail.com>
2023-08-23 16:25:13 -0600

    add: build and other minor tweaks

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter } from 'react-router-dom'
 
 // const router = createBrowserRouter([
 // {
@@ -23,6 +23,8 @@ import { createBrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={"/Lsarj_Website/"}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )

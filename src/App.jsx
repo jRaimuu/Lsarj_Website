67f44219e943146851d5848e9bd1656352e8e6b1
jRaimuu <liamsarj4@gmail.com>
2023-08-24 02:57:55 -0600

    Trying HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ import EndFooter from './components/EndFooter';
 function App() {
   return (
     <>
-      <HashRouter>
+      <HashRouter >
         <Routes>
-          <Route path="/Lsarj_Website/home" element={<MainPageV2 />} />
+          <Route path="/" element={<MainPageV2 />} />
         </Routes>
       </HashRouter>
       {/* <MainPageV2/> */}
--- src/main.jsx
+++ src/main.jsx
@@ -23,11 +23,8 @@ import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <BrowserRouter basename={"/Lsarj_Website/"}>
-    <App /> 
-  </BrowserRouter> */}
-    <HashRouter basename="/Lsarj_Website/">
+    {/* <BrowserRouter basename="/Lsarj_Website/"> */}
       <App />
-    </HashRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>,
 )

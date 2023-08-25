74fb386b94f9b11da29eadb7dc2bcb56bee5c389
jRaimuu <liamsarj4@gmail.com>
2023-08-24 22:15:54 -0600

    Adding path specifier to Hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ import EndFooter from './components/EndFooter';
 function App() {
   return (
     <>
-      <HashRouter >
+      <HashRouter basename='/Lsarj_Website'>
         <Routes>
-          <Route path="/" element={<MainPageV2 />} />
+          <Route path="/Lsarj_Website" element={<MainPageV2 />} />
         </Routes>
       </HashRouter>
       {/* <MainPageV2/> */}
